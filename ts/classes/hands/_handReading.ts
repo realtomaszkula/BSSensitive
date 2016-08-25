c0ca2c5438fe875e5221b8a487bde0cb00e7f745
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-25 13:52:49 -0700

    Add class builder and rank search basic layout

--- ts/classes/hands/_handReading.ts
+++ ts/classes/hands/_handReading.ts
@@ -4,6 +4,7 @@ import { TwoPair } from './TwoPair'
 import { Trips } from './Trips'
 import { Straight } from './Straight'
 import { Flush } from './Flush'
+import { FullHouse } from './FullHouse'
 import { Quads } from './Quads'
 import { StraightFlush } from './StraightFlush'
 
@@ -12,7 +13,7 @@ export class HandRankSearch {
   private _sortedValues: CardValue[];
 
   constructor( private _cards: Card[] ) { 
-    this.figureOutHandRank();
+    this._handRank = this.figureOutHandRank();
   }
 
   get result(): HandRank {
@@ -23,35 +24,70 @@ export class HandRankSearch {
     this._sortedValues = this._cards.map( card =>  card.value  ).sort()
   }
 
+  private searchRanks =  {
+    isStraighFlush(): boolean {
+      return true
+    },
+
+    isQuads(): boolean {
+      return true
+    },
+    isFullHouse(): boolean {
+      return true
+    },
+    isFlush(): boolean {
+      return true
+    },
+    isStraight(): boolean {
+      return true
+    },
+    isTrips(): boolean {
+      return true
+    },
+    isTwoPair(): boolean {
+      return true
+    },
+    isPair(): boolean {
+      return true
+    }
+  }
+
+  private classBuilder =  {
+    StraighFlush(params): StraightFlush {
+      return new StraightFlush()
+    },
+    Quads(params): Quads {
+      return new Quads()
+    },
+    FullHouse(params): FullHouse {
+      return new FullHouse()
+    },
+    Flush(params): Flush {
+      return new Flush()
+    },
+    Straight(params): Straight {
+      return new Straight()
+    },
+    Trips(params): Trips {
+      return new Trips()
+    },
+    TwoPair(params): TwoPair {
+      return new TwoPair()
+    },
+    Pair(params): Pair {
+      return new Pair()
+    }
+  }
+
   private figureOutHandRank() {
-    this.sortValues();
+    let classes = ['Pair', 'TwoPair', 'Trips', 'Straight', 'Flush', 'FullHouse', 'Quads', 'StraightFlush']
+    for (let className of classes) {
+      let { found, params } =  this.searchRanks['is' + [className]]
+      if( found ) return this.classBuilder[className](params)
+    }
   }
 
-  private isStraighFlush(): boolean {
-    return true
-  }
 
-  private isQuads(): boolean {
-    return true
-  }
-  private isFullHouse(): boolean {
-    return true
-  }
-  private isFlush(): boolean {
-    return true
-  }
-  private isStraight(): boolean {
-    return true
-  }
-  private isTrips(): boolean {
-    return true
-  }
-  private isTwoPair(): boolean {
-    return true
-  }
-  private isPair(): boolean {
-    return true
-  }
 
 
 }
--- ts/classes/hands/_interfaces.ts
+++ ts/classes/hands/_interfaces.ts
@@ -1,3 +1,12 @@
+import { Pair } from './Pair'
+import { TwoPair } from './TwoPair'
+import { Trips } from './Trips'
+import { Straight } from './Straight'
+import { Flush } from './Flush'
+import { FullHouse } from './FullHouse'
+import { Quads } from './Quads'
+import { StraightFlush } from './StraightFlush'
+
 type Suit = 'spade' | 'club' | 'diamond' | 'hearts'
 type HandRank = Pair | TwoPair | Trips | Straight | Flush | FullHouse | Quads | StraightFlush
 
