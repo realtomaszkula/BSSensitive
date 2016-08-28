ed99b748f5ef95eed582a578efa3b0e21ed8fc99
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-28 13:27:02 -0700

    Pass quads spec

--- ts/classes/hands/Quads.ts
+++ ts/classes/hands/Quads.ts
@@ -1,20 +1,31 @@
-import { Card, CardClass, HandParams,  Suit, CardValue, QuadsParams } from './_interfaces'
+import { HandClass, Card, CardClass, HandParams,  Suit, CardValue, QuadsParams } from './_interfaces'
 import { Hand } from './_hand'
 
 export class Quads extends Hand implements HandClass {
 
-  private _quads: [Card, Card, Card, Card]
-  private _kicker: Card
+  private _quads: CardValue;
+  private _kicker: CardValue;
 
   constructor( params: QuadsParams ) {
     super(params)
+    this._quads = params.quads;
   }
 
-  get quads(): Card {
-    return this._quads[0]
+  get quads(): CardValue {
+    return this._quads;
+  }
+
+  setKickers() {
+    super.setKickers(this._quads);
   }
 
   resolveConflict(other: Quads): number {
-    return this.compare( { my: this.quads, other: other.quads } )
+    if (this.quads === other.quads) {
+      this.setKickers();
+      other.setKickers();
+      return this.checkKickers(other);
+    }
+
+    return (this.quads > other.quads) ? 1 : -1;
   }
 }
