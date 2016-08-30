56669ad1997260e35e2fda5da85456858436b35d
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-30 04:05:26 -0700

    Add and pass tests for 3 cards NLHE

--- ts/specs/theBestHandNLHE_spec.ts
+++ ts/specs/theBestHandNLHE_spec.ts
@@ -291,7 +291,6 @@ describe('TheBestHand', function() {
         });
       });
 
-      
       describe('and hand including wheel straightflush', function() {
         let theBestHand: TheBestHand;
         beforeEach(function() {
@@ -309,6 +308,137 @@ describe('TheBestHand', function() {
 
     
     describe('when given 5 cards', function() {
+      describe('and hand of high card', function() {
+      let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [ card('aceOfSpades'), card('jackOfDiamonds') ]
+          let boardCards: Flop = [card('kingOfClubs'), card('queenOfspades'), card('threeOfSpades') ]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class HighCard', function() {
+          expect(theBestHand.result instanceof HighCard ).toBe(true)
+        });
+      });
+
+      describe('and hand including pair', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('aceOfDiamonds')]
+          let boardCards: Flop = [card('kingOfClubs'), card('queenOfspades'), card('threeOfSpades') ]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class Pair', function() {
+          expect(theBestHand.result instanceof Pair ).toBe(true)
+        });
+      });
+
+      describe('and hand including two pair', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('aceOfDiamonds')]
+          let boardCards: Flop = [ card('kingOfClubs'), card('kingOfspades'), card('threeOfSpades')] 
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class TwoPair', function() {
+          expect(theBestHand.result instanceof TwoPair ).toBe(true)
+        });
+      });
+
+      describe('and hand including trips', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('aceOfDiamonds')]
+          let boardCards: Flop = [card('aceOfClubs'), card('queenOfspades'), card('threeOfSpades')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class Trips', function() {
+          expect(theBestHand.result instanceof Trips ).toBe(true)
+        });
+      });
+
+      describe('and hand including straight', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('kingOfDiamonds')]
+          let boardCards: Flop = [card('queenOfClubs'), card('jackOfspades'), card('tenOfSpades')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class Straight', function() {
+          expect(theBestHand.result instanceof Straight ).toBe(true)
+        });
+      });
+
+      describe('and hand including wheel straight', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('fiveOfDiamonds')]
+          let boardCards: Flop = [card('duceOfspades'), card('threeOfSpades'), card('fourOfClubs')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class Straight', function() {
+          expect(theBestHand.result instanceof Straight ).toBe(true)
+        });
+      });
+
+      describe('and hand including flush', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('kingOfSpades')]
+          let boardCards: Flop = [card('eightOfSpades'), card('threeOfSpades'), card('fiveOfSpades')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class Flush', function() {
+          expect(theBestHand.result instanceof Flush ).toBe(true)
+        });
+      });
+
+      describe('and hand including fullhouse', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('aceOfDiamonds')]
+          let boardCards: Flop = [card('aceOfClubs'), card('queenOfspades'), card('queenOfClubs')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class FullHouse', function() {
+          expect(theBestHand.result instanceof FullHouse ).toBe(true)
+        });
+      });
+
+      describe('and hand including quads', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('aceOfDiamonds')]
+          let boardCards: Flop = [card('aceOfClubs'), card('aceOfHearts'),card('queenOfClubs')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class Quads', function() {
+          expect(theBestHand.result instanceof Quads ).toBe(true)
+        });
+      });
+
+      describe('and hand including straightflush', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+          let playerCards: HoldemHoleCards = [ card('aceOfSpades'), card('kingOfSpades')]
+          let boardCards: Flop = [ card('jackOfSpades'), card('tenOfSpades'), card('queenOfSpades') ]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class StraightFlush', function() {
+          expect(theBestHand.result instanceof StraightFlush ).toBe(true)
+        });
+      });
+
+      describe('and hand including wheel straightflush', function() {
+        let theBestHand: TheBestHand;
+        beforeEach(function() {
+        let playerCards: HoldemHoleCards = [card('aceOfSpades'), card('fiveOfSpades')]
+          let boardCards: Flop = [card('threeOfSpades'),  card('duceOfSpades'), card('fourOfSpades')]
+          theBestHand = new TheBestHand({playerCards: playerCards, boardCards: boardCards})
+        });
+        it('should return instance of class StraightFlush', function() {
+          expect(theBestHand.result instanceof StraightFlush ).toBe(true)
+        });
+      });
     }); // desc 3 cards
     
     
