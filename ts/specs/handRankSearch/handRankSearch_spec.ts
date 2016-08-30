6237924885399c8064998416e4c57fb0666f3da0
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-30 14:07:32 -0700

    Refactor hand rank search spec and add reverse spec

--- ts/specs/handRankSearch/handRankSearch_spec.ts
+++ ts/specs/handRankSearch/handRankSearch_spec.ts
@@ -12,126 +12,147 @@ import { Card, CardClass, HandParams,  Suit, CardValue } from './../../classes/h
 import { card } from './../helpers/methods'
 
 describe('HandRankSearch', function() {
+let hand: HandRankSearch;
+let params: [Card, Card, Card, Card];
   
   describe('when given hand with high card', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [card('aceOfSpades'), card('jackOfDiamonds'), card('kingOfClubs'), card('queenOfClubs'), card('duceOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [card('aceOfSpades'), card('jackOfDiamonds'), card('kingOfClubs'), card('queenOfClubs'), card('duceOfClubs')]
     });
-      
     it('instance should return instance of class Pair', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof HighCard ).toBe(true)
+    });
+    it('instance should return instance of class Pair', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof HighCard ).toBe(true)
     });
   });
 
   describe('when given hand including pair', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [card('aceOfSpades'), card('aceOfDiamonds'), card('kingOfClubs'), card('queenOfClubs'), card('duceOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [card('aceOfSpades'), card('aceOfDiamonds'), card('kingOfClubs'), card('queenOfClubs'), card('duceOfClubs')]
     });
-      
     it('instance should return instance of class Pair', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof Pair ).toBe(true)
+    });
+    it('instance should return instance of class Pair', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof Pair ).toBe(true)
     });
   });
 
   describe('when given hand including two pair', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [card('aceOfSpades'), card('aceOfDiamonds'), card('kingOfClubs'), card('kingOfHearts'), card('duceOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [card('aceOfSpades'), card('aceOfDiamonds'), card('kingOfClubs'), card('kingOfHearts'), card('duceOfClubs')]
     });
-      
     it('instance should return instance of class two pair', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof TwoPair ).toBe(true)
+    });
+    it('instance should return instance of class two pair', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof TwoPair ).toBe(true)
     });
   });
 
   describe('when given hand including trips', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [card('aceOfSpades'), card('aceOfDiamonds'), card('aceOfClubs'), card('kingOfHearts'), card('duceOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [card('aceOfSpades'), card('aceOfDiamonds'), card('aceOfClubs'), card('kingOfHearts'), card('duceOfClubs')]
     });
-      
     it('instance should return instance of class trips', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof Trips ).toBe(true)
+    });
+    it('instance should return instance of class trips', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof Trips ).toBe(true)
     });
   });
 
   describe('when given hand including straight', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [ card('aceOfSpades'), card('kingOfDiamonds'), card('queenOfClubs'), card('jackOfClubs'), card('tenOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [ card('aceOfSpades'), card('kingOfDiamonds'), card('queenOfClubs'), card('jackOfClubs'), card('tenOfClubs')]
     });
-      
     it('instance should return instance of class straight', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof Straight ).toBe(true)
+    });
+    it('instance should return instance of class straight', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof Straight ).toBe(true)
     });
   });
 
   describe('when given hand including wheel straight', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [ card('aceOfSpades'), card('duceOfDiamonds'), card('threeOfClubs'), card('fourOfClubs'), card('fiveOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [ card('aceOfSpades'), card('duceOfDiamonds'), card('threeOfClubs'), card('fourOfClubs'), card('fiveOfClubs')]
     });
-      
     it('instance should return instance of class straight', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof Straight ).toBe(true)
+    });
+    it('instance should return instance of class straight', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof Straight ).toBe(true)
     });
   });
 
   describe('when given hand including flush', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [ card('aceOfClubs'), card('kingOfClubs'), card('queenOfClubs'), card('jackOfClubs'), card('duceOfClubs')]
-      hand = new HandRankSearch(params) 
+      params = [ card('aceOfClubs'), card('kingOfClubs'), card('queenOfClubs'), card('jackOfClubs'), card('duceOfClubs')] 
     });
-      
     it('instance should return instance of class flush', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof Flush ).toBe(true)
+    });
+    it('instance should return instance of class flush', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof Flush ).toBe(true)
     });
   });
 
   describe('when given hand including fullhouse', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [card('aceOfSpades'), card('aceOfDiamonds'), card('aceOfClubs'), card('kingOfHearts'), card('kingOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [card('aceOfSpades'), card('aceOfDiamonds'), card('aceOfClubs'), card('kingOfHearts'), card('kingOfClubs')]
     });
-      
     it('instance should return instance of class fullhouse', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof FullHouse ).toBe(true)
+    });
+    it('instance should return instance of class fullhouse', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof FullHouse ).toBe(true)
     });
   });
 
   describe('when given hand including quads', function() {
-  let hand: HandRankSearch;
-  let params: Card[] = [card('aceOfSpades'), card('aceOfDiamonds'), card('aceOfClubs'), card('aceOfHearts'), card('duceOfClubs')]
     beforeEach(function() {
-      hand = new HandRankSearch(params )
+      params = [card('aceOfSpades'), card('aceOfDiamonds'), card('aceOfClubs'), card('aceOfHearts'), card('duceOfClubs')]
     });
-      
     it('instance should return instance of class quads', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof Quads ).toBe(true)
+    });
+    it('instance should return instance of class quads', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof Quads ).toBe(true)
     });
   });
 
   describe('when given hand including straightflush', function() {
-  let hand: HandRankSearch;
     beforeEach(function() {
-      let params: Card[] = [ card('aceOfClubs'), card('kingOfClubs'), card('queenOfClubs'), card('jackOfClubs'), card('tenOfClubs')]
-      hand = new HandRankSearch(params)
+      params = [ card('aceOfClubs'), card('kingOfClubs'), card('queenOfClubs'), card('jackOfClubs'), card('tenOfClubs')]
     });
-      
     it('instance should return instance of class straightflush', function() {
+      hand = new HandRankSearch(params)
+      expect(hand.result instanceof StraightFlush ).toBe(true)
+    });
+    it('instance should return instance of class straightflush', function() {
+      hand = new HandRankSearch(params.reverse())
       expect(hand.result instanceof StraightFlush ).toBe(true)
     });
   });
 
-    
 });
