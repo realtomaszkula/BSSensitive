575552100277c6f9c55fe77052d9e68e478998ed
Tomasz Kula <tomaszkula07@gmail.com>
2016-09-03 14:57:27 +0200

    Refactor CardValue spec to use permutations

--- ts/specs/TextureTypes/CardValue_spec.ts
+++ ts/specs/TextureTypes/CardValue_spec.ts
@@ -1,6 +1,6 @@
 import { TextureTypes } from './../../classes/boards/TextureTypes/_TextureTypes'
 import { CardValueTextureType } from './../../classes/boards/TextureTypes/CardValue'
-import { card } from './../helpers/methods'
+import { card, permutationOfBoards } from './../helpers/methods'
 import { customMatchers } from './../helpers/customMatchers'
 import { Texture, CardValue } from './../../classes/_interfaces'
 
@@ -16,80 +16,41 @@ describe('CardValueTextureType', function() {
   });
     
   describe('when given one broadway cards and two rags', function() {
-    beforeEach(function() {
-      values = [CardValue.ace, CardValue.duce, CardValue.three]
-      textureObject = new CardValueTextureType({ values: values})
-    });
-    it('should set type to OneBroadway', function() {
-      expect(textureObject).toBeOfTextureType('OneBroadway')
-    });
-    it('should NOT set type to TwoBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('TwoBroadway')
-    });
-    it('should NOT set type to ThreeBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('ThreeBroadway')
-    });
-    it('should NOT set type to ZeroBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('ZeroBroadway')
-    }); 
+    values = [CardValue.ace, CardValue.duce, CardValue.three]
+    for(let permutation of permutationOfBoards(values)) {
+      it('should set type to OneBroadway', function() {
+        textureObject = new CardValueTextureType({ values: permutation})
+        expect(textureObject).toBeOfTextureType('OneBroadway')
+      });
+    }
   });
 
   describe('when given two broadway cards and one rag', function() {
-    beforeEach(function() {
-      values = [CardValue.ace, CardValue.king, CardValue.three]
-      textureObject = new CardValueTextureType({ values: values})
-    });
-    it('should NOT set type to OneBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('OneBroadway')
-    });
-    it('should set type to TwoBroadway', function() {
-      expect(textureObject).toBeOfTextureType('TwoBroadway')
-    });
-    it('should NOT set type to ThreeBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('ThreeBroadway')
-    });
-    it('should NOT set type to ZeroBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('ZeroBroadway')
-    }); 
-  });
+    values = [CardValue.ace, CardValue.king, CardValue.three]
+    for(let permutation of permutationOfBoards(values)) {
+      it('should set type to TwoBroadway', function() {
+        textureObject = new CardValueTextureType({ values: permutation})
+        expect(textureObject).toBeOfTextureType('TwoBroadway')
+      });
+    }
 
   describe('when given three broadway cards and zero rags', function() {
-    beforeEach(function() {
-      values = [CardValue.ace, CardValue.king, CardValue.queen]
-      textureObject = new CardValueTextureType({ values: values})
-    });
-    it('should NOT set type to OneBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('OneBroadway')
-    });
-    it('should NOT set type to TwoBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('TwoBroadway')
-    });
-    it('should set type to ThreeBroadway', function() {
-      expect(textureObject).toBeOfTextureType('ThreeBroadway')
-    });
-    it('should NOT set type to ZeroBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('ZeroBroadway')
-    }); 
-  });
+    values = [CardValue.ace, CardValue.king, CardValue.queen]
+    for(let permutation of permutationOfBoards(values)) {
+      it('should set type to ThreeBroadway', function() {
+        textureObject = new CardValueTextureType({ values: permutation})
+        expect(textureObject).toBeOfTextureType('ThreeBroadway')
+      });
+    }
 
   describe('when given zero broadway cards and three rags', function() {
-    beforeEach(function() {
-      values = [CardValue.four, CardValue.three, CardValue.duce]
-      textureObject = new CardValueTextureType({ values: values})
-    });
-    it('should NOT set type to OneBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('OneBroadway')
-    });
-    it('should NOT set type to TwoBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('TwoBroadway')
-    });
-    it('should NOT type to ThreeBroadway', function() {
-      expect(textureObject).not.toBeOfTextureType('ThreeBroadway')
-    });
-    it('should set type to ZeroBroadway', function() {
-      expect(textureObject).toBeOfTextureType('ZeroBroadway')
-    }); 
-  });
+    values = [CardValue.four, CardValue.three, CardValue.duce]
+    for(let permutation of permutationOfBoards(values)) {
+      it('should set type to ZeroBroadway', function() {
+        textureObject = new CardValueTextureType({ values: permutation})
+        expect(textureObject).toBeOfTextureType('ZeroBroadway')
+      }); 
+    }
   
 });
   
